77aac95b5a3907537f351e2922faf5c20dcdd5ee
davidlondonor <davidlucky@gmail.com>
2021-06-25 15:02:38 -0500

    save the details

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,27 +1,50 @@
-import React, { useState, useEffect} from "react"
+import React, { useState, useEffect, useReducer, useMemo } from "react"
+
+const initialState = {
+  favorites: []
+}
+
+const favoriteReducer = (state, action ) => {
+  switch(action.type) {
+    case 'ADD_TO_FAVORITES':
+      return {
+        ...state,
+        favorites: [...state.favorites, action.payload]
+      };
+      default:
+        return state;
+  }
+}
 
 const Characters = () => {
-
     const [character, setCharacter] = useState([])
+    const [favorites, dispatch] = useReducer(favoriteReducer, initialState)
+    const [search, setSetSearch] = useState('')
 
     useEffect(() => {
         fetch('https://rickandmortyapi.com/api/character')
         .then(response => response.json())
         .then( data => setCharacter(data.results))
     }, [])
+
+    const handleClick = favorite => {
+      dispatch({ type: 'ADD_TO_FAVORITES', payload: favorites })
+    }
+
     return (
-        <>
+    <>
        <div className="characters">
+         {favorites.favorites.map(favorites =>(
+           <li key={favorites.id}>{favorites.name}</li>
+         ))}
            {character.map(character => (
-             <h2>{character.name}</h2>
+             <div className="item" key={character.id }>
+               <h2>{character.name}</h2>
+               <button type='button' onClick={() => handleClick(character)}>Agregar a Favoritos</button>
+             </div>
            ))}
        </div>
-         <div className="characters">
-         {character.map(character => (
-           <p>{character.status}</p>
-         ))}
-     </div>
-     </>
+       </>
     )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,17 @@
-import React, { useState} from "react"
+import React, { useState, useContext } from "react"
+import ThemeContext from "../context/ThemeContext"
 
 const Header= ()=> {
 const [darkMode, setDarkMode ] = useState(false)
+const color = useContext(ThemeContext)
 const handleClick = () => {
     setDarkMode(!darkMode)
 }
 
-
     return (
         <>   
 <div className="header">
-    <h1>React Hooks</h1>
+    <h1 style={{color}}>React Hooks</h1>
     <button type="button" onClick={handleClick}>{darkMode ? "dark mode" : "ligth mode"}</button>
     <button type="button" onClick={() => setDarkMode(!darkMode)}>{darkMode ? "dark mode 2" : "ligth mode 2"}</button>
 
